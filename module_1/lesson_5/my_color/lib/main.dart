0e485a3e5e8a4274a936bc2fe913aeff5073cb16
W-ZhenLS <w.zhenls20@gmail.com>
2022-07-10 20:04:51 +0800

    feat: added solution for module_1 lesson_5 (switch image)

--- module_1/lesson_5/my_color/lib/main.dart
+++ module_1/lesson_5/my_color/lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
 import 'dart:math' as math show Random;
+
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      title: 'My Color',
+      title: 'My Image',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -28,31 +29,40 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Color Switching'),
+        title: const Text('Image Switching'),
       ),
       body: Container(
-        color: containerColor[counter],
+        color: Colors.yellow[200],
+        child: Center(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+            child: Image.network(
+              superheroUrls[counter],
+              width: 280,
+              height: 400,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            counter = math.Random().nextInt(containerColor.length);
+            counter = math.Random().nextInt(superheroUrls.length);
           });
         },
-        tooltip: 'Switch Color',
+        tooltip: 'Switch Image',
         child: const Icon(Icons.refresh),
       ),
     );
   }
 }
 
-// Given a list of container color
-List<Color> containerColor = [
-  Colors.red,
-  Colors.orange,
-  Colors.yellow,
-  Colors.green,
-  Colors.blue,
-  Colors.indigo,
-  Colors.purple,
+final List<String> superheroUrls = [
+  'https://images.unsplash.com/photo-1635805737707-575885ab0820?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+  'https://images.unsplash.com/photo-1594463750939-ebb28c3f7f75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  'https://images.unsplash.com/photo-1640499900704-b00dd6a1103a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+  'https://images.unsplash.com/photo-1624213111452-35e8d3d5cc18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=726&q=80',
+  'https://images.unsplash.com/photo-1559535332-db9971090158?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
 ];
