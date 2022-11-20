e2e5bc952bc95d43df9b9dedb1f07251601ee7ef
Shahidan Majid <shahidanmajid@Shahidans-MacBook-Pro.local>
2022-11-20 11:44:22 +0800

    update API key storage and .gitignore.

--- module_3/assignment_1/my_weather/lib/data/network/rest_api_service.dart
+++ module_3/assignment_1/my_weather/lib/data/network/rest_api_service.dart
@@ -1,18 +1,23 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+
 import 'package:my_weather/data/model/weather.dart';
 
+///Create api_key.dart file at the root of lib/ folder.
+///Create a constant variable as follows and assign to your API_KEY.
+///const WEATHER_API_KEY = '2777xxxxxxxx';
+///Insert /lib/api_key.dart in .gitignore to hide your API key from version control (Github).
+import 'package:my_weather/api_key.dart';
+
 class RestApiService {
-  static const String baseLink = 'https://api.openweathermap.org/data/2.5/weather';
-  static const String apiKey = 'a3fddeb923ffaf43991e52e271477941';
+  static const String baseLink =
+      'https://api.openweathermap.org/data/2.5/weather';
 
   /// API call for get weather by city id
-  /// 
   Future<Weather> getWeatherByCityId(String cityId) async {
     final response = await http.get(
-      Uri.parse('$baseLink?units=metric&appid=$apiKey&id=$cityId')
-    );
+        Uri.parse('$baseLink?units=metric&appid=$WEATHER_API_KEY&id=$cityId'));
     if (response.statusCode == 200) {
       final raw = jsonDecode(response.body);
       return Weather.fromJson(raw);
